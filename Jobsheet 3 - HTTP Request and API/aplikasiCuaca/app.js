16ec0a17e3fc5665546cb24dcda63918c32deade
syadzaoktifani <oktifanisyadza@gmail.com>
2026-01-08 23:57:32 +0700

    done

--- Jobsheet 3 - HTTP Request and API/aplikasiCuaca/app.js
+++ Jobsheet 3 - HTTP Request and API/aplikasiCuaca/app.js
@@ -1,84 +1,28 @@
-// const request = require('postman-request');
+// const request = require('postman-request')
+// const url = 'http://api.weatherstack.com/current?access_key=55eb3af056e988b9dc5c89a397a69218&query=-0.1388965641899718, 100.55187679685'
+// request({ url: url }, (error, response) => {
+//     // console.log(response)
+//     const data = JSON.parse(response.body)
+//     console.log(data)
+//     // console.log(data.current)
+//     console.log(data.current.temperature)
+// })
 
-// const url = `http://api.weatherstack.com/current?access_key=9c1b3a3b00ed8d8e3cb8452370bc20d4&query=Padang Barat`;
-
-// request(
-//     {url},
-//     (err, res) => {
-//         if (err)
-//             return console.log('Gagal mendapatkan data cuaca.');
-
-//         const { request, location } = JSON.parse(res.body);
-//         const query = request.query;
-//         const place_name = `${location.name}, ${location.region}, ${location.country}`;
-//         const place_type = request.type;
-        
-//         console.log(`data yang anda cari adalah: ${query}`)
-//         console.log(`data yang ditemukan adalah: ${place_name}`)
-//         console.log(`tipe lokasi adalah: ${place_type}`)
-//     }
-// );
-
-
-// const request = require('postman-request');
-
-// const place = encodeURIComponent('Padang Barat');
-// const geocodeURL = `https://nominatim.openstreetmap.org/search?q=${place}&format=json&limit=3`;
-
-// request(
-//     {
-//         url: geocodeURL,
-//         json: true,
-//         headers: { 'User-Agent': 'NetworkProgramming-Project/1.0' }
-//     },
-//     (err, res) => {
-//         if(err || res.body.length === 0)
-//             return console.log('Gagal mendapatkan lokasi');
-
-//         const { display_name, type } = res.body[0];
-//         console.log(`data yang anda cari adalah: ${decodeURIComponent(place)}`);
-//         console.log(`data yang ditemukan adalah: ${display_name}`);
-//         console.log(`tipe lokasi adalah: ${type}`);
-//     }
-// );
-
-
-const request = require('postman-request');
-
-const place = encodeURIComponent('Padang Barat');
-const geocodeURL = `https://nominatim.openstreetmap.org/search?q=${place}&format=json&limit=3`;
-
+const request = require('postman-request')
+const url = 'http://api.weatherstack.com/current?access_key=55eb3af056e988b9dc5c89a397a69218&query=-0.1388965641899718, 100.55187679685'
 request(
-    {
-        url: geocodeURL, json: true,
-        headers: { 'User-Agent': 'NetworkProgramming-Project/1.0' }
-    },
+    {url},
     (err, res) => {
-        if(err || res.body.length === 0)
-            return console.log('Gagal mendapatkan lokasi');
+        if (err)
+            return console.log('Gagal mendapatkan data cuaca.');
 
-        const { lat, lon, display_name, type } = res.body[0];
-        console.log(`Koordinat lokasi anda adalah ${lat}, ${lon}`);
-        console.log(`Data yang anda cari adalah: ${decodeURIComponent(place)}`);
-        console.log(`Data yang ditemukan adalah: ${display_name}`);
-        console.log(`Tipe lokasi adalah: ${type}`);
-
-        const weatherURL = `http://api.weatherstack.com/current?access_key=9c1b3a3b00ed8d8e3cb8452370bc20d4&query=${lat},${lon}`;
-
-        request(
-            {url: weatherURL},
-            (err, res) => {
-                if (err || res.body.error)
-                    return console.log('Gagal mendapatkan data cuaca.');
-
-                const { current, location } = JSON.parse(res.body);
-                console.log(
-                    `Saat ini suhu di ${location.name} mencapai ${current.temperature} derajat celcius.`
-                );
-                console.log(
-                    `Kemungkinan terjadinya hujan adalah ${current.precip}%`
-                );
-            }
-        );
+        const { request, location } = JSON.parse(res.body);
+        const query = request.query;
+        const place_name = `${location.name}, ${location.region}, ${location.country}`;
+        const place_type = request.type;
+        
+        console.log(`data yang anda cari adalah: ${query}`)
+        console.log(`data yang ditemukan adalah: ${place_name}`)
+        console.log(`tipe lokasi adalah: ${place_type}`)
     }
 );
--- Jobsheet 3 - HTTP Request and API/aplikasiCuaca/cekCuaca.js
+++ Jobsheet 3 - HTTP Request and API/aplikasiCuaca/cekCuaca.js
@@ -1,11 +1,6 @@
-const request = require('postman-request');
-const urlCuaca = `http://api.weatherstack.com/current?access_key=9c1b3a3b00ed8d8e3cb8452370bc20d4&query=-0.8970925271039868,%20100.35069537156373`;
-
-request(
-    {url: urlCuaca},
-    (err, res) => {
-        const { current } = JSON.parse(res.body);
-        console.log(`Cuaca saat ini: ${current.weather_descriptions[0]}`);
-        console.log(`Saat ini suhu diluar mencapai ${current.temperature} derajat celcius. Kemungkinan terjadinya hujan adalah ${current.precip} %`);
-    }
-);
+const request = require('postman-request')
+const urlCuaca = 'http://api.weatherstack.com/current?access_key=55eb3af056e988b9dc5c89a397a69218&query=-0.13886437777698835, 100.55195189870547&units=f'
+request({ url: urlCuaca, json: true }, (error, response) => {
+    console.log('Saat ini suhu di luar mencapai ' + response.body.current.temperature +
+        '°f. Kemungkinan terjadinya hujan adalah ' + response.body.current.precip + '%')
+})
